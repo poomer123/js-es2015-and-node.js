02eb01449d2d336dc36140f2d19d565d6ab2a3cb
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-03-05 16:56:26 +0700

    Arrow Function เขียนฟังก์ชั่นแบบใหม่ สั้น กระชับกว่า

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
+console.log('Arrow Function เขียนฟังก์ชั่นแบบใหม่ สั้น กระชับกว่า')
+
+var greet = function(name, message) {
+    return message + name
+}
+
+var arrowGreet = (name, message) => {
+    return message + name
+}
+
+var arrowGreet2 = (name, message) => message + name
+
+var arrowGreet3 = message => message
+
+var square = x => x * x
 
 
+var person = {
+    name: 'Luna',
 
-let a = 'Test Hello 333'
+    handleMessage: function(message, handler) {
+        handler(message)
+    },
 
-console.log(a)
+    greet: function() {
+        this.handleMessage('Hi', message => console.log(message + this.name))
+    }
+}
 
+
+person.greet()
