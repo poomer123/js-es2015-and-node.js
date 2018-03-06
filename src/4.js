55b021efedd6dbcd3629328454b277145aa2115f
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-03-06 15:44:38 +0700

    Rest Spread คู่หูใหม่ที่คุณจะขาดไม่ได้

---
+++ src/4.js
@@ -0,0 +1,13 @@
+const string = `test ES6
+test es6
+test`
+
+console.warn(string);
+
+
+
+const name = 'Jo'
+const time = 'today'
+const greet = `Hi ${name + 'joe'}, How are you ${time}?`
+
+console.warn(greet);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
-const string = `test ES6
-test es6
-test`
+const arr = [4,5,6]
+const append = [1,2,3, ...arr]
 
-console.warn(string);
-
+console.warn(append)
 
 
-const name = 'Jo'
-const time = 'today'
-const greet = `Hi ${name + 'joe'}, How are you ${time}?`
+const arr1 = ['a', 'b', 'c']
+const arr2 = ['d', 'e', 'f']
+arr1.push(...arr2)
+console.warn(arr1)
 
-console.warn(greet);
+
+const arr3 = [1,2,3]
+const copy = arr3.slice()
+console.warn(copy)
+copy.push(4)
+console.warn(arr3)
+console.warn(copy)
+
+
+
+function add(x,y,z) {
+    return x+y+z
+}
+
+
+const numbers = [1,2,3]
+const sum = add(1,2,3)
+console.log(sum)
+
+
+const sum1 = add(...numbers)
+console.log(sum1)
+
+// Rest params
+function howManyArgs(...args) {
+    console.log(args.length)
+    console.log(args)
+}
+howManyArgs()
+howManyArgs(4)
+howManyArgs(4,5)
+
+
+function multiply(multiplier, ...array5) {
+    console.log(multiplier)
+    console.log(array5)
+    return array5.map(e => multiplier * e)
+}
+const result = multiply(2, 100, 200, 300)
+console.log(result)
